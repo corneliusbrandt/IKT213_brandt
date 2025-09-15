c5cbe0462183bfb0792bc81fdae193539fe0b32c
corneliusbrandt <bc7970@mci4me.at>
2025-09-15 16:23:44 +0200

    start assignment 3

---
+++ assignment_3/main.py
@@ -0,0 +1,12 @@
+import cv2
+
+
+img = cv2.imread("assignment_3\lambo.png")
+img_blur = cv2.GaussianBlur(img, (3,3), sigmaX=0)
+
+def sobel_edge_detection(image):
+    sobelxy = cv2.Sobel(src=image, ddepth=cv2.CV_64F, dx=1, dy=1, ksize=1)
+    cv2.imshow('Sobel XY', sobelxy)
+
+
+sobel_edge_detection(img)
