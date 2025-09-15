7f7c3480cffec217a551ca4f1a834c36b783950e
corneliusbrandt <bc7970@mci4me.at>
2025-09-15 16:26:26 +0200

    minor changes

--- assignment_3/main.py
+++ assignment_3/main.py
@@ -7,6 +7,7 @@ img_blur = cv2.GaussianBlur(img, (3,3), sigmaX=0)
 def sobel_edge_detection(image):
     sobelxy = cv2.Sobel(src=image, ddepth=cv2.CV_64F, dx=1, dy=1, ksize=1)
     cv2.imshow('Sobel XY', sobelxy)
+    cv2.waitKey(0)
 
 
 sobel_edge_detection(img)
